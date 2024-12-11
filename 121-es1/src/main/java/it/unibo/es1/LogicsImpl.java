53f464ef90133f18fb8566c37d7704ed3f16dbbd
Razzino Adam Paolo <adampaolo.razzino@studio.unibo.it>
2024-12-11 15:31:25 +0100

    Creata una classe generica setter per inizializzare le liste inizialmente corretti errori nei metodi hit e toQuit, implementato il metodo result

--- 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
+++ 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
@@ -2,6 +2,7 @@ package it.unibo.es1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class LogicsImpl implements Logics {
@@ -9,12 +10,16 @@ public class LogicsImpl implements Logics {
 	private final int size;
 	private List<Integer> value;
 	private List<Boolean> enable;
+
+	private final String IN = "<<";
+	private final String END = ">>";
+	private final String MID = "|";
 	
 
 	public LogicsImpl(int size) {
 		this.size = size;
-		this.value = new ArrayList<>(size);
-		this.enable = new ArrayList<>(size);
+		this.value = null;
+		this.enable = null;
 	}
 
 	@Override
@@ -22,22 +27,26 @@ public class LogicsImpl implements Logics {
 		return this.size;
 	}
 
+	private <T> List<T> setter(T defaultValue){
+		final List<T> temp = new ArrayList<>(this.size());
+		for (int count = 0; count < this.size(); count++){
+			temp.add(defaultValue);
+		}
+		return temp;
+	}
+
 	@Override
 	public List<Integer> values() {
-		 if(this.value.isEmpty()){
-			for (int count = 0; count < this.size(); count++){
-				value.add(count);
-			}
+		 if(this.value == null){
+			this.value = this.setter(0);
 		 }
 		return value;
 	}
 
 	@Override
 	public List<Boolean> enablings() {
-		 if(this.enable.isEmpty()){
-			for (int count = 0; count < this.size(); count++){
-				enable.add(true);
-			}
+		 if(this.enable == null){
+			this.enable = this.setter(true);
 		 }
 		return enable;
 	}
@@ -45,22 +54,26 @@ public class LogicsImpl implements Logics {
 	@Override
 	public int hit(int elem) {
 		int temp = this.values().get(elem) + 1;
-		this.values().set(elem, temp < this.size ? temp : 0);
+		if(temp == this.size()){
+			temp = this.size();
+			this.enablings().set(elem, false);
+		}
+		this.values().set(elem, temp);
 		return this.values().get(elem);
 	}
 
 	@Override
 	public String result() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'result'");
+		return IN + values().stream().map(Object :: toString).collect(Collectors.joining(MID)) + END;
 	}
 
 	@Override
 	public boolean toQuit() {
-		while (values().iterator().hasNext() ) {
-			if(!values().get(0).equals(values().iterator())){
-				return false;
-			}
+		if(values().stream().distinct().map(a -> 1).count() != 1){
+			return false;
+		}
+		for(int count = 0; count < this.size(); count++){
+			this.enablings().set(count, false);
 		}
 		return true;
 	}
