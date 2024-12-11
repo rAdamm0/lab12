a6f3375581b3557058ce1f36e60448233d573698
Razzino Adam Paolo <adampaolo.razzino@studio.unibo.it>
2024-12-11 10:26:48 +0100

    Rimossa la lista di Jbuttons e implementato le liste dei valori Integer e valori di enabling

--- 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
+++ 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
@@ -3,32 +3,37 @@ package it.unibo.es1;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.JButton;
 
 public class LogicsImpl implements Logics {
 
+	private final int size;
 	
 
 	public LogicsImpl(int size) {
-		final List<JButton> buttons = new ArrayList<>(size);
+		this.size = size;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'size'");
+		return this.size;
 	}
 
 	@Override
 	public List<Integer> values() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'values'");
+		final List<Integer> value = new ArrayList<>(this.size());
+		for ( int count = 0; count < this.size(); count++){
+			value.set(count, count);
+		}
+		return value;
 	}
 
 	@Override
 	public List<Boolean> enablings() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'enablings'");
+		final List<Boolean> enable = new ArrayList<>(this.size());
+		for (int count = 0; count < this.size(); count++){
+			enable.set(count, true);
+		}
+		return enable;
 	}
 
 	@Override
