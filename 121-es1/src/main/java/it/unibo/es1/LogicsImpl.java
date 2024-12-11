d74ccf734313ad1950734420ee3839aacde35c1d
Razzino Adam Paolo <adampaolo.razzino@studio.unibo.it>
2024-12-11 12:03:48 +0100

    Fatto qualche cambiamento sulle liste dei valori e implementazione di metodi hit e toQuit

--- 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
+++ 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
@@ -22,7 +22,7 @@ public class LogicsImpl implements Logics {
 	public List<Integer> values() {
 		final List<Integer> value = new ArrayList<>(this.size());
 		for ( int count = 0; count < this.size(); count++){
-			value.set(count, count);
+			value.add(count);
 		}
 		return value;
 	}
@@ -31,15 +31,16 @@ public class LogicsImpl implements Logics {
 	public List<Boolean> enablings() {
 		final List<Boolean> enable = new ArrayList<>(this.size());
 		for (int count = 0; count < this.size(); count++){
-			enable.set(count, true);
+			enable.add(true);
 		}
 		return enable;
 	}
 
 	@Override
 	public int hit(int elem) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'hit'");
+		int temp = this.values().get(elem) + 1;
+		this.values().set(elem, temp < this.size ? temp : 0);
+		return this.values().get(elem);
 	}
 
 	@Override
@@ -50,7 +51,11 @@ public class LogicsImpl implements Logics {
 
 	@Override
 	public boolean toQuit() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'toQuit'");
+		while (values().iterator().hasNext() ) {
+			if(!values().get(0).equals(values().iterator())){
+				return false;
+			}
+		}
+		return true;
 	}
 }
