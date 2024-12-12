297be31906c5c545fdde10988efc638b8e4db88a
Razzino Adam Paolo <adampaolo.razzino@studio.unibo.it>
2024-12-12 11:44:10 +0100

    made a small change in the typing of the hit method to a boolean and implemented the method itself

--- 122-es2/src/main/java/it/unibo/es2/Logics.java
+++ 122-es2/src/main/java/it/unibo/es2/Logics.java
@@ -1,5 +1,26 @@
 package it.unibo.es2;
 
 public interface Logics {
-
+	
+	/**
+	 * @return the number of buttons (except Print)
+	 */
+	int size();
+	
+	/**
+	 * @return if a value has been clicked or not 
+	 */
+	boolean [][] values();
+	
+	/**
+	 * @return the new value a button should show after being pressed
+	 */
+	boolean hit(Pair<Integer,Integer> elem);
+	
+	/**
+	 * @return whether it is time to quit
+	 */
+	boolean toQuit();
 }
+
+
--- 122-es2/src/main/java/it/unibo/es2/LogicsImpl.java
+++ 122-es2/src/main/java/it/unibo/es2/LogicsImpl.java
@@ -1,5 +1,7 @@
 package it.unibo.es2;
 
+import java.util.Arrays;
+
 public class LogicsImpl implements Logics{
 
     final int size;
@@ -13,16 +15,24 @@ public class LogicsImpl implements Logics{
         return this.size;
     }
 
-    @Override
-    public boolean[][] values() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'values'");
+    private void matrixSet(int size){
+        value = new boolean[size][size];
     }
 
     @Override
-    public int hit(int elem) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'hit'");
+    public boolean[][] values() {
+        if (this.value.length == 0){
+            matrixSet(this.size());
+        }
+        return this.value;
+    }
+
+    @Override
+    public boolean hit(Pair<Integer,Integer> elem) {
+        if(elem.getX()<this.size() && elem.getY()<this.size){
+            return this.values()[elem.getX()][elem.getY()] = this.values()[elem.getX()][elem.getY()] == true ? false : true;
+        } 
+        throw new ArrayIndexOutOfBoundsException();
     }
 
     @Override
